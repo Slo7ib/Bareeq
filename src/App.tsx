f2a47de197907439c285a178c9e44900febf1e6a
Saleh Blhmr <83185094+Slo7ib@users.noreply.github.com>
2026-04-17 02:36:29 +0300

    U

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
-
+// Dont forget to add Supabase and Wassnger to the project
 function App() {
   return (
     <>
-      <h1 className="text-3xl bg-amber-600 font-bold underline">
+      <h1 className="bg-amber-600 text-3xl font-bold underline">
         Hello world!
       </h1>
     </>
